78a05648e8cea90f967541924f0a4423c6ce6274
andrewens <andrewens4@gmail.com>
2025-01-04 20:52:28 -0700

    Count characters in a string

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-
+# private
 def get_word_count(string):
     return len(string.split())
 
 
+def count_each_char(string):
+    char_count = dict()
+
+    for char in string:
+        char = char.lower()
+
+        if char not in char_count:
+            char_count[char] = 1
+            continue
+        
+        char_count[char] += 1
+
+    return char_count
+
+
+# public
 def main():
     with open('books/frankenstein.txt') as f:
         file_contents = f.read()
-        # print(file_contents)
-        print(get_word_count(file_contents))
+        print(count_each_char(file_contents))
 
 
 main()
